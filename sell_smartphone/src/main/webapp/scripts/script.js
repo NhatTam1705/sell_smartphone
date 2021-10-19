694dd4fbef86ace719b6054fb3c671e1ab7d178d
HoQuocVinh <89761777+HoQuocVinh@users.noreply.github.com>
2021-10-20 00:45:39 +0700

    update 0:45 20/10/2021

--- sell_smartphone/src/main/webapp/scripts/script.js
+++ sell_smartphone/src/main/webapp/scripts/script.js
@@ -1,13 +1,11 @@
-function call() {
-    var header = document.getElementById('htv-navbar');
-    var menu = document.getElementById('menu');
-    menu.onclick = function () {
-        var isClosed = header.clientHeight === 83;
-        if (isClosed) {
-            header.style.height = 'auto';
-        }
-        else {
-            header.style.height = '83px';
-        }
+var header = document.getElementById('htv-navbar');
+var menu = document.getElementById('menu');
+menu.onclick = function () {
+    var isClosed = header.clientHeight === 83;
+    if (isClosed) {
+        header.style.height = 'auto';
+    }
+    else {
+        header.style.height = '83px';
     }
 }
