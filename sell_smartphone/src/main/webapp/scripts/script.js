2079667dacf7c58d32e5309a10dacbb0614b3f53
HoQuocVinh <89761777+HoQuocVinh@users.noreply.github.com>
2021-10-21 23:07:40 +0700

    update 23:07 21/10/2021

--- sell_smartphone/src/main/webapp/scripts/script.js
+++ sell_smartphone/src/main/webapp/scripts/script.js
@@ -1,22 +1,45 @@
-// var header = document.getElementById('htv-navbar');
 // var menu = document.getElementById('menu');
-// var headerHeight = header.clientHeight;
+// var test = document.getElementByClassName('hidden');
+// var count = 1;
 // menu.onclick = function () {
-//     var isClosed = header.clientHeight === headerHeight;
-//     if (isClosed) {
-//         header.style.height = 'auto';
-//         // navigation.style.height = null;
-//     }
-//     else {
-//         header.style.height = null;
+//     var i;
+//     if (count === 1) {
+//         for (i = 0; i < test.length; i++)
+//             test[i].style.display = 'none';
+//         count = 0;
+//     } else {
+//         for (i = 0; i < test.length; i++)
+//             test[i].style.display = 'block';
+//         count = 1;
 //     }
 // }
+
+
+var navigation = document.getElementsByClassName('hidden-nav')
+var count = 0
+function menu() {
+    var i;
+    if (count === 0){
+        for (i = 0; i < navigation.length; i++) {
+            navigation[i].style.display = 'none'
+        }
+        count = 1;
+    }
+    else{
+        for (i = 0; i < navigation.length; i++) {
+            navigation[i].style.display = 'flex'
+        }
+        count = 0;
+    }
+
+}
 
 //Search
 
 const navSearch = document.querySelector('.js-nav-search')
 const navRight = document.querySelector('.js-nav-right')
 const htvNavBar = document.querySelector('.js-htv-navbar')
+
 function showSearchBox() {
     navSearch.classList.add('show-nav-search')
     navRight.classList.add('hide-nav-right')
@@ -28,4 +51,3 @@ function hideSearchBox() {
     navRight.classList.remove('hide-nav-right')
     htvNavBar.classList.remove('hide-htv-navbar')
 }
-
