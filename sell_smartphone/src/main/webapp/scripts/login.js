289d5f45a010dbb11bcf03b5a4544e0e18ccdf71
HoQuocVinh <89761777+HoQuocVinh@users.noreply.github.com>
2021-10-25 22:30:55 +0700

    update 22:30 25/11/2021

--- sell_smartphone/src/main/webapp/scripts/login.js
+++ sell_smartphone/src/main/webapp/scripts/login.js
@@ -1,16 +1,26 @@
+var click = 0
 {
     function textUser(){
         var textUser = document.getElementById('text-user');
         var textPhone = document.getElementById('text-phone')
-        var click = 0;
-        if(click ===0){
+        if(click === 0){
             textUser.classList.add('blur-text')
+            textUser.classList.remove('unblur-text')
             textPhone.classList.add('unblur-text')
+            textPhone.classList.remove('blur-text')
+            click = 1
         }
     }
     function textPhone(){
-        var textUser = document.getElementById('text-user')
+        var textUser = document.getElementById('text-user');
         var textPhone = document.getElementById('text-phone')
+        if(click === 1){
+            textUser.classList.add('unblur-text')
+            textUser.classList.remove('blur-text')
+            textPhone.classList.add('blur-text')
+            textPhone.classList.remove('unblur-text')
+            click = 0
+        }
     }
 }   
 
